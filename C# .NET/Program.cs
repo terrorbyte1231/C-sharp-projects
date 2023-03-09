52f60a56280d6bb385fa00edc1d21f2cdea5a779
terrorbyte1231 <sevrin.work@gmail.com>
2023-03-10 02:14:47 +0300

    Calc1 first commit

--- C# .NET/Program.cs
+++ C# .NET/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data.SqlTypes;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_.NET
 {
@@ -12,70 +6,133 @@ namespace C_.NET
     {
         static void Main(string[] args)
         {
-            float rubblesInWallet;
-            float dollarsInWallet;
-            float exchangeCurrencyCount;
+            Console.WriteLine("CALC Everything!!!");
 
-            int rubToUsd = 64, usdToRub = 67;
-
-            string desireOperation;
-
-            Console.WriteLine("Добро пожаловать в обменник валют!");
-
-            Console.Write("Введите баланс рублей: ");
-            rubblesInWallet = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Введите баланс долларов: ");
-            dollarsInWallet = Convert.ToSingle(Console.ReadLine());
-
-            Console.WriteLine("Выберите необходимую операцию.");
-            Console.WriteLine("1 - обменять рубли на доллары");
-            Console.WriteLine("2 - обменять доллары на рубли");
-            Console.Write("Ваш выбор: ");
-
-            desireOperation = Console.ReadLine();
-
-            switch (desireOperation)
+            while (true)
             {
-                case "1":
-                    Console.Write("Сколько рублей вы хотите обменять? ");
-                    exchangeCurrencyCount = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Что вы хотите посчитать?");
+                Console.WriteLine("1 - обычный калькулятор");
+                Console.WriteLine("2 - обменник курс доллар/рубль");
+                Console.WriteLine("3 - конвертер миль в км");
+                Console.WriteLine("4 - индекс массы тела");
+                Console.WriteLine("5 - необходимое число калорий в день");
+                Console.WriteLine("6 - високосность года");
+                Console.Write("Введите ваш выбор: ");
 
-                    if (rubblesInWallet >= exchangeCurrencyCount)
-                    {
-                        rubblesInWallet -= exchangeCurrencyCount;
-                        dollarsInWallet += exchangeCurrencyCount / rubToUsd;
-                        Console.WriteLine($"Вы обмениваете {exchangeCurrencyCount} рублей на {exchangeCurrencyCount / rubToUsd} долларов");
-                    }
-                    else
-                    {
-                        Console.WriteLine("У вас недостаточно рублей для данной операции");
-                    }
+                int workModeChoose; // Выбор режима работы программы
+                workModeChoose = Convert.ToInt32(Console.ReadLine());
 
-                    Console.WriteLine($"В вашем кошельке {rubblesInWallet} рублей и {dollarsInWallet} долларов");
-                    break;
-                case "2":
-                    Console.Write("Сколько долларов вы хотите обменять? ");
-                    exchangeCurrencyCount = Convert.ToInt32(Console.ReadLine());
+                switch (workModeChoose)
+                {
+                    case 1:
+                        string conditionCalc1 = "Да";
+                        while (conditionCalc1 == "Да")
+                        {
+                            Console.WriteLine("'+' - сложение");
+                            Console.WriteLine("'-' - вычитание");
+                            Console.WriteLine("'*' - умножение");
+                            Console.WriteLine("'/' - деление");
+                            Console.WriteLine("'%' - остаток от деления");
+                            Console.Write("Выберите операцию: ");
 
-                    if (dollarsInWallet >= exchangeCurrencyCount)
-                    {
-                        dollarsInWallet -= exchangeCurrencyCount;
-                        rubblesInWallet += exchangeCurrencyCount * usdToRub;
-                        Console.WriteLine($"Вы обмениваете {exchangeCurrencyCount} долларов на {exchangeCurrencyCount * usdToRub} рублей");
-                    } 
-                    else
-                    {
-                        Console.WriteLine("У вас недостаточно долларов для данной операции");
-                    }
+                            string operationChoose;
+                            operationChoose = Console.ReadLine();
 
-                    Console.WriteLine($"В вашем кошельке {rubblesInWallet} рублей и {dollarsInWallet} долларов");
-                    break;
-                default:
-                    Console.WriteLine("Введено некорректное значение!");
-                    break;
+                            Console.WriteLine("Введите числа которые хотите посчитать");
+
+                            Console.Write("Введите первое число: ");
+                            float firstNum;
+                            firstNum = Convert.ToSingle(Console.ReadLine());
+
+                            Console.Write("\n" + "Введите второе число: ");
+                            float secondNum;
+                            secondNum = Convert.ToSingle(Console.ReadLine());
+
+                            switch (operationChoose)
+                            {
+                                case "+":
+                                    Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}");
+                                    break;
+
+                                case "-":
+                                    Console.WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum}");
+                                    break;
+
+                                case "*":
+                                    Console.WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}");
+                                    break;
+
+                                case "/":
+                                    Console.WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum}");
+                                    break;
+
+                                case "%":
+                                    Console.WriteLine($"Остаток от деления {firstNum} на {secondNum} равен {firstNum % secondNum}");
+                                    break;
+                            }
+
+                            bool calcContinue = true; // Переменная состояния для обработки ошибки. Если пользователь вводит некорректное значение, вопрос задается повторно.
+                            while (calcContinue)
+                            {
+                                Console.WriteLine("Желаете продолжить работать с калькулятором чисел?");
+                                conditionCalc1 = Console.ReadLine().ToLower();
+
+                                switch (conditionCalc1)
+                                {
+                                    case "да":
+                                    case "yes":
+                                    case "y":
+                                    case "д":
+                                        Console.WriteLine("Перенаправление в начало калькулятора");
+                                        conditionCalc1 = "да";
+                                        calcContinue = false;
+                                        break;
+
+                                    case "нет":
+                                    case "н":
+                                    case "не":
+                                    case "no":
+                                    case "n":
+                                        Console.WriteLine("Перенаправление в начальное меню");
+                                        conditionCalc1 = "нет";
+                                        calcContinue = false;
+                                        break;
+
+                                    default:
+                                        Console.WriteLine($"Я не знаю слова {conditionCalc1.ToUpper()}");
+                                        break;
+                                }
+
+                            }
+                        }
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    default:
+
+                        break;
+                }
             }
-
-
         }
     }
 }
